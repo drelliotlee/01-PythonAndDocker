92f895e5b2a9f9a5f5f42ee655760eb340ac5454
Elliot Lee <drelliotlee@gmail.com>
2024-12-21 01:28:26 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# Load the Iris dataset into a table using pandas
+from sklearn.datasets import load_iris
+import pandas as pd
+
+def main():
+    iris = load_iris()
+
+    iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+
+    print(iris_df)
+    print("foo")
+
+if __name__ == "__main__":
+    main()
